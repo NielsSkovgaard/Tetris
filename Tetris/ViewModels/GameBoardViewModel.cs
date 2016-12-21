468571dfd166f290fde873aec8f56695477565c8
Niels Skovgaard <niels_skovgaard@hotmail.com>
2016-12-22 00:43:03 +0100

    Cleaning up of comments and long variable names.

--- Tetris/ViewModels/GameBoardViewModel.cs
+++ Tetris/ViewModels/GameBoardViewModel.cs
@@ -15,23 +15,20 @@ namespace Tetris.ViewModels
 
         public bool IsGamePaused { get; private set; }
 
-        // TimeSpans and Timers:
-        // - for holding down a key to repeat a command (move left/right or rotate) every x milliseconds
-        private readonly TimeSpan _timeSpanMoveCurrentPieceLeftOrRight = TimeSpan.FromMilliseconds(100); // 100 ms = 10 FPS
-        private readonly TimeSpan _timeSpanRotateCurrentPiece = TimeSpan.FromMilliseconds(250); // 250 ms = 4 FPS
-        private readonly DispatcherTimer _timerMoveCurrentPieceLeftOrRight = new DispatcherTimer();
-        private readonly DispatcherTimer _timerRotateCurrentPiece = new DispatcherTimer();
-
-        // - to add gravity to CurrentPiece. It will move down faster when soft dropping (by holding down the down key)
-        // Every level increases the speed by 18%. Notice that the max level speed is equal to the soft drop speed
-        private readonly int[] _moveCurrentPieceDownIntervalsInMilisecondsPerLevel = { 800, 656, 538, 441, 362, 297, 243, 199, 164, 134, 110, 90, 74, 61, 50 };
-
-        private readonly TimeSpan _timeSpanMoveCurrentPieceDownSoftDrop = TimeSpan.FromMilliseconds(50); // 50 ms = 20 FPS
-        private readonly DispatcherTimer _timerMoveCurrentPieceDown = new DispatcherTimer();
-
-        // - to count the number of seconds the game has been running
-        private readonly TimeSpan _timeSpanSecondsGameHasBeenRunning = TimeSpan.FromMilliseconds(1000); // 1000 ms = 1 FPS
-        private readonly DispatcherTimer _timerSecondsGameHasBeenRunning = new DispatcherTimer();
+        // TimeSpans and DispatcherTimers:
+        // Left/right: Repeat every x milliseconds while holding down the left and/or right button
+        // Rotate: Repeat every x milliseconds while holding down the rotate button
+        // Down: Either gravity (repeat every x milliseconds) or soft dropping (repeat every x milliseconds while holding down the down button)
+        // Statistics.Time: The time the game has been running
+        private readonly TimeSpan _timeSpanLeftRight = TimeSpan.FromMilliseconds(100); // 100 ms = 10 FPS
+        private readonly TimeSpan _timeSpanRotate = TimeSpan.FromMilliseconds(250); // 250 ms = 4 FPS
+        private readonly TimeSpan _timeSpanDownSoftDrop = TimeSpan.FromMilliseconds(50); // 50 ms = 20 FPS
+        private readonly TimeSpan _timeSpanStatisticsTime = TimeSpan.FromMilliseconds(1000); // 1000 ms = 1 FPS
+        private readonly int[] _downGravityIntervalsPerLevelMilliseconds = { 800, 656, 538, 441, 362, 297, 243, 199, 164, 134, 110, 90, 74, 61, 50 }; // 18% speed increase per level
+        private readonly DispatcherTimer _timerLeftRight = new DispatcherTimer();
+        private readonly DispatcherTimer _timerRotate = new DispatcherTimer();
+        private readonly DispatcherTimer _timerDown = new DispatcherTimer();
+        private readonly DispatcherTimer _timerStatisticsTime = new DispatcherTimer();
 
         // For handling that _timerMovePieceLeftOrRight runs when moving left or right; otherwise, the timer should be stopped
         private bool _isLeftKeyDown;
@@ -47,16 +44,14 @@ namespace Tetris.ViewModels
             _gameBoard = gameBoard;
             _statistics = statistics;
 
-            // DispatcherTimer.Interval
-            _timerMoveCurrentPieceLeftOrRight.Interval = _timeSpanMoveCurrentPieceLeftOrRight;
-            _timerRotateCurrentPiece.Interval = _timeSpanRotateCurrentPiece;
-            _timerSecondsGameHasBeenRunning.Interval = _timeSpanSecondsGameHasBeenRunning;
-
-            // DispatcherTimer.Tick
-            _timerMoveCurrentPieceLeftOrRight.Tick += (sender, args) => TryMoveCurrentPieceLeftOrRight();
-            _timerRotateCurrentPiece.Tick += (sender, args) => _gameBoard.TryRotateCurrentPiece();
-            _timerMoveCurrentPieceDown.Tick += (sender, args) => MoveCurrentPieceDownProcess();
-            _timerSecondsGameHasBeenRunning.Tick += (sender, args) => _statistics.IncrementTime();
+            // Timers
+            _timerLeftRight.Interval = _timeSpanLeftRight;
+            _timerRotate.Interval = _timeSpanRotate;
+            _timerStatisticsTime.Interval = _timeSpanStatisticsTime;
+            _timerLeftRight.Tick += (sender, args) => TryMoveLeftOrRight();
+            _timerRotate.Tick += (sender, args) => _gameBoard.TryRotateCurrentPiece();
+            _timerDown.Tick += (sender, args) => MoveDownProcess();
+            _timerStatisticsTime.Tick += (sender, args) => _statistics.IncrementTime();
 
             StartNewGame();
         }
@@ -79,10 +74,10 @@ namespace Tetris.ViewModels
             _isSoftDropping = false;
 
             // Timers
-            _timerMoveCurrentPieceLeftOrRight.Stop();
-            _timerRotateCurrentPiece.Stop();
-            _timerMoveCurrentPieceDown.Stop();
-            _timerSecondsGameHasBeenRunning.Stop();
+            _timerLeftRight.Stop();
+            _timerRotate.Stop();
+            _timerDown.Stop();
+            _timerStatisticsTime.Stop();
         }
 
         public void ResumeGame()
@@ -90,14 +85,14 @@ namespace Tetris.ViewModels
             IsGamePaused = false;
 
             // Timers
-            _timerMoveCurrentPieceDown.Interval = GetMoveCurrentPieceDownTimerIntervalBasedOnLevel();
-            _timerMoveCurrentPieceDown.Start();
-            _timerSecondsGameHasBeenRunning.Start();
+            _timerDown.Interval = DownGravityIntervalBasedOnLevel();
+            _timerDown.Start();
+            _timerStatisticsTime.Start();
         }
 
-        private TimeSpan GetMoveCurrentPieceDownTimerIntervalBasedOnLevel()
+        private TimeSpan DownGravityIntervalBasedOnLevel()
         {
-            return TimeSpan.FromMilliseconds(_moveCurrentPieceDownIntervalsInMilisecondsPerLevel[_statistics.Level - 1]);
+            return TimeSpan.FromMilliseconds(_downGravityIntervalsPerLevelMilliseconds[_statistics.Level - 1]);
         }
 
         public void KeyDown(Key key, bool isRepeat)
@@ -112,28 +107,28 @@ namespace Tetris.ViewModels
                     _isLeftKeyDown = true;
                     _leftKeyHasPriority = true;
                     _gameBoard.TryMoveCurrentPieceLeft();
-                    _timerMoveCurrentPieceLeftOrRight.Start();
+                    _timerLeftRight.Start();
                     break;
                 case Key.Right:
                 case Key.D:
                     _isRightKeyDown = true;
                     _leftKeyHasPriority = false;
                     _gameBoard.TryMoveCurrentPieceRight();
-                    _timerMoveCurrentPieceLeftOrRight.Start();
+                    _timerLeftRight.Start();
                     break;
                 case Key.Up:
                 case Key.W:
                     _gameBoard.TryRotateCurrentPiece();
-                    _timerRotateCurrentPiece.Start();
+                    _timerRotate.Start();
                     break;
                 case Key.Down:
                 case Key.S:
                     _isSoftDropping = true;
-                    MoveCurrentPieceDownProcess();
-                    _timerMoveCurrentPieceDown.Interval = _timeSpanMoveCurrentPieceDownSoftDrop;
+                    MoveDownProcess();
+                    _timerDown.Interval = _timeSpanDownSoftDrop;
                     break;
                 case Key.Space:
-                    // TODO: Hard drop the CurrentPiece (and handle that it won't result in clicking the buttons)
+                    // TODO: Hard drop CurrentPiece (and handle that it won't result in clicking the buttons)
                     break;
             }
         }
@@ -151,7 +146,7 @@ namespace Tetris.ViewModels
                     _leftKeyHasPriority = false;
 
                     if (!_isRightKeyDown)
-                        _timerMoveCurrentPieceLeftOrRight.Stop();
+                        _timerLeftRight.Stop();
                     break;
                 case Key.Right:
                 case Key.D:
@@ -159,21 +154,21 @@ namespace Tetris.ViewModels
                     _leftKeyHasPriority = true;
 
                     if (!_isLeftKeyDown)
-                        _timerMoveCurrentPieceLeftOrRight.Stop();
+                        _timerLeftRight.Stop();
                     break;
                 case Key.Up:
                 case Key.W:
-                    _timerRotateCurrentPiece.Stop();
+                    _timerRotate.Stop();
                     break;
                 case Key.Down:
                 case Key.S:
                     _isSoftDropping = false;
-                    _timerMoveCurrentPieceDown.Interval = GetMoveCurrentPieceDownTimerIntervalBasedOnLevel();
+                    _timerDown.Interval = DownGravityIntervalBasedOnLevel();
                     break;
             }
         }
 
-        private void TryMoveCurrentPieceLeftOrRight()
+        private void TryMoveLeftOrRight()
         {
             if (_leftKeyHasPriority)
                 _gameBoard.TryMoveCurrentPieceLeft();
@@ -181,7 +176,7 @@ namespace Tetris.ViewModels
                 _gameBoard.TryMoveCurrentPieceRight();
         }
 
-        private void MoveCurrentPieceDownProcess()
+        private void MoveDownProcess()
         {
             // TODO: Check inside this method that if not all CurrentPiece.Blocks fit on the GameBoard, then don't move it down, but it's game over
             bool canMoveDown = _gameBoard.TryMoveCurrentPieceDown();
@@ -224,7 +219,7 @@ namespace Tetris.ViewModels
                     _statistics.UpdateOnCompletingRows(numberOfCompleteRows);
                     int levelAfter = _statistics.Level;
                     if (levelAfter > levelBefore && !_isSoftDropping)
-                        _timerMoveCurrentPieceDown.Interval = GetMoveCurrentPieceDownTimerIntervalBasedOnLevel();
+                        _timerDown.Interval = DownGravityIntervalBasedOnLevel();
 
                     _gameBoard.OnChanged();
                 }
